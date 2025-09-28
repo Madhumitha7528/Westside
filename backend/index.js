be424e22b21039e46a4f3a9f7a52d3821649e920
Madhumitha7528 <madhumitha7528@gmail.com>
2025-09-28 17:11:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res) => {
     res.json({
         success: 1,
-        image_url: `https://westside-frontend.onrender.com/images/${req.file.filename}`
+        image_url: `https://westside-backend-2kuo.onrender.com/images/${req.file.filename}`
     })
 })
 
